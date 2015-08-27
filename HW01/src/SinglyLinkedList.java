a195503ab1ad6ca961c492f4a744f20fde89b5c9
Travis Clement <travisclement@yahoo.com>
2015-08-27 17:06:23 -0400

    uploading to git for back really

--- HW01/src/SinglyLinkedList.java
+++ HW01/src/SinglyLinkedList.java
@@ -12,62 +12,163 @@ public class SinglyLinkedList<T> implements LinkedListInterface<T> {
 
     @Override
     public void addAtIndex(int index, T data) {
+        if (data == null) {
+            throw new IllegalArgumentException("Data cannot be null");
+        }
+        if (index < 0 || index > size) {
+            throw new IndexOutOfBoundsException("Index number "
+                    + "negative or too large");
+        }
+        LinkedListNode<T> newnode = new LinkedListNode<T>(data, null);
+        if (index == 0) {
+            head = newnode;
+            size++;
+        } else {
+            LinkedListNode<T> oldindex = head;
+            for (int i = 1; i < index; i++) {
+                oldindex = oldindex.getNext();
+            }
+            newnode.setNext(oldindex.getNext());
+            oldindex.setNext(newnode.getNext());
+            size++;
+        }
 
     }
 
     @Override
     public T get(int index) {
+        if (index < 0 || index > size) {
+            throw new IndexOutOfBoundsException("Index number "
+                    + "negative or too large");
+        }
+        if (index == 0) {
+            return head.getData();
+        } else {
+            LinkedListNode<T> getnode = head;
+            for (int i = 1; i < index; i++) {
+                getnode = getnode.getNext();
+            }
+            return getnode.getData();
+        }
 
     }
 
+
     @Override
     public T removeAtIndex(int index) {
+        if (index < 0 || index > size) {
+            throw new IndexOutOfBoundsException("Index number "
+                    + "negative or too large");
+        }
+
+        LinkedListNode<T> current = head;
+        LinkedListNode<T> previous = null;
+        if (index == 0) {
+            head = head.getNext();
+            size--;
+            return current.getData();
+        } else {
+            for (int i = 1; i < index; i++) {
+                previous = current;
+                current  = current.getNext();
+            }
+            previous.setNext(current.getNext());
+            size--;
+            return current.getData();
+        }
 
     }
 
     @Override
     public void addToFront(T data) {
-
+        if (data == null) {
+            throw new IllegalArgumentException("Cannot add null data to list");
+        }
+        LinkedListNode<T> newnode = new LinkedListNode<T>(data, head);
+        head = newnode;
+        size++;
     }
 
     @Override
     public void addToBack(T data) {
+        if (data == null) {
+            throw new IllegalArgumentException("Cannot add null data to list");
+        }
+        LinkedListNode<T> newnode = new LinkedListNode<T>(data);
+        if (head == null) {
+            head = newnode;
+            size++;
+        } else {
+            LinkedListNode<T> temp = head;
+            while (temp.getNext() != null) {
+                temp = temp.getNext();
+            }
+            temp.setNext(newnode);
+            size++;
+        }
 
     }
 
     @Override
     public T removeFromFront() {
+        if (head == null) {
+            return null;
+        } else {
+            LinkedListNode<T> temp = head;
+            head = head.getNext();
+            size--;
+            return temp.getData();
+        }
+        return head.getData();
 
     }
 
     @Override
     public T removeFromBack() {
 
+        if (head == null) {
+            return null;
+        }
+        LinkedListNode<T> current = head;
+        LinkedListNode<T> previous = null;
+
+        while (current.getNext() != null) {
+            previous = current;
+            current = current.getNext();
+        }
+        previous.setNext(null);
+        size--;
+        return current.getData();
     }
+
 
     @Override
     public int removeFirstOccurrence(T data) {
-
+        return 0;
     }
 
-    @Override
-    public Object[] toArray() {
-
-    }
+  //  @Override
+   // public Object[] toArray() {
+//
+   // }
 
     @Override
     public boolean isEmpty() {
-
+        if (size == 0) {
+            return true;
+        }
+        return false;
     }
 
     @Override
     public int size() {
-
+        return size;
     }
 
     @Override
     public void clear() {
-
+        head = null;
+        size = 0;
     }
 
     @Override
